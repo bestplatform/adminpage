b4d363a88ef49a354b3eb8995427b51e29387206
KyoungHieLee <aisanano1@gmail.com>
2016-10-12 23:05:54 +0900

    miniboard prototype

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
+//connect database
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open", function() {
+    console.log("DB connected!");
+});
+db.on("error", function(err) {
+    console.log("DB ERROR :", err);
+});
+
+//model setting
+var postSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    body: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: Date
+});
+var Post = mongoose.model('post', postSchema);
+
+//view setting
+app.set("view engine", 'ejs');
+
+//set middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+//set routes
+app.get('/posts', function(req, res) {
+    Post.find({}, function(err, posts) {
+        if (err) return res.json(
+          {success: false, message: err});
+        res.json({success: true, data: posts});
+    });
+});
+
+
+//start server
+
+app.listen(3000, function() {
+    console.log('Service On!');
+});
